f9e0491d91b3c42231fd9c78c3acde62090dd88c
Rebbekkah <90262374+Rebbekkah@users.noreply.github.com>
2021-10-20 15:42:38 +0200

    Add files via upload

--- Kaggle.py
+++ Kaggle.py
@@ -1,47 +1,101 @@
 import PIL as pil
 import os, os.path
+import pandas as pd
+import numpy as np
 import cv2
 
 
-test_normal = []
-test_pneu = []
-train_normal = []
-train_pneu = []
-val_normal = []
-val_pneu = []
 
 
 def folder(path_test, path_train, path_val):
-	list_path = [path_test, path_train, path_val]
-	for path in list_path:
-		if os.path.exists(os.path.join(path, "NORMAL")):
-			new_path = os.path.join(path, "NORMAL")
-			for file in os.listdir(new_path):
-				if str(new_path) == os.path.join(path_test, "NORMAL"):
-					test_normal.append(file)						
-				elif str(new_path) == os.path.join(path_train, "NORMAL"):
-					train_normal.append(file)
-				elif str(new_path) == os.path.join(path_val, "NORMAL"):
-					val_normal.append(file)
-		elif os.path.exists(os.path.join(path, "PNEUMONIA")):
-			new_path = os.path.join(path, "PNEUMONIA")
-			for file in os.listdir(new_path):
-				if str(new_path) == os.path.join(path_test, "PNEUMONIA"):
-					test_pneu.append(file)		
-				elif str(new_path) == os.path.join(path_train, "PNEUMONIA"):
-					print(new_path)
-					train_pneu.append(file)
-				elif str(new_path) == os.path.join(path_val, "PNEUMONIA"):
-					print(new_path)
-					val_pneu.append(file)
-	#print(val_normal, val_pneu)
-	#return test_normal, test_pneu, train_normal, train_pneu, val_normal, val_pneu
+    test_normal = []
+    test_pneu = []
+    train_normal = []
+    train_pneu = []
+    val_normal = []
+    val_pneu = []
 
+    list_path = [path_test, path_train, path_val]
+    for path in list_path:
+        if os.path.exists(os.path.join(path, "NORMAL")):
+            new_path = os.path.join(path, "NORMAL")
+            for file in os.listdir(new_path):
+                if str(new_path) == os.path.join(path_test, "NORMAL"):
+                    test_normal.append(file)						
+                elif str(new_path) == os.path.join(path_train, "NORMAL"):
+                    train_normal.append(file)
+                elif str(new_path) == os.path.join(path_val, "NORMAL"):
+                    val_normal.append(file)
+        if os.path.exists(os.path.join(path, "PNEUMONIA")):
+            new_path = os.path.join(path, "PNEUMONIA")
+            for file in os.listdir(new_path):
+                if str(new_path) == os.path.join(path_test, "PNEUMONIA"):
+                    test_pneu.append(file)		
+                elif str(new_path) == os.path.join(path_train, "PNEUMONIA"):
+                    train_pneu.append(file)
+                elif str(new_path) == os.path.join(path_val, "PNEUMONIA"):
+                    val_pneu.append(file)
+    #print(val_normal, val_pneu)
+    return test_normal, test_pneu, train_normal, train_pneu, val_normal, val_pneu
+
+
+
+def as_df(test_normal, test_pneu, train_normal, train_pneu, val_normal, val_pneu) :
+    print(len(test_normal), len(test_pneu))
+
+    #liste = [test_normal, test_pneu, train_normal, train_pneu, val_normal, val_pneu]
+        
+    test = test_normal + test_pneu
+    train = train_normal + train_pneu
+    val = val_normal + val_pneu
+    
+
+    liste = [test, train, val]
+    for list in liste :
+        cls = []
+        print(list)
+        #joining_normal = list.join('_normal')
+        #joining_pneu = list.join('_pneu')
+        for index in range(len(list.join('_normal'))) :
+            cls.append("normal")
+        for index in range(len(list.join('_pneu'))) :
+            cls.append("pneumonia")
+        
+        df_ = pd.DataFrame({'class' : cls, 'image' : list})
+        df_.join(list)
+
+    
+
+
+
+    '''
+    cls = []
+    for i in range(len(test_normal)) :
+        cls.append("normal")
+    for i in range(len(test_pneu)) :
+        cls.append("pneumonia")
+
+    print("length of class {}".format(len(cls)))
+
+    df_test = pd.DataFrame({'class' : cls, 'image' : test})
+    print(df_test)
+    #print("this is the test list : {}".format(test))
+
+    #df_test = pd.DataFrame({'normal' : test_normal, 'pneumonial' : test_pneu})
+    #print(df_test)
+    '''
+'''
+    liste = [test_normal, test_pneu, train_normal, train_pneu, val_normal, val_pneu]
+    for l in liste :
+        l = pd.Dataframe(l)
+    print(test_normal)
+
+    return test_normal, test_pneu, train_normal, train_pneu, val_normal, val_pneu
+'''
 
 '''
 import cv2
 import os
-
 def load_images_from_folder(folder):
     images = []
     for filename in os.listdir(folder):
@@ -55,17 +109,17 @@ def load_images_from_folder(folder):
 #samefile(f1, f2)
 
 if __name__ == "__main__":
-	path_test = "/Users/rgoulanc/Desktop/Rebecca/FAC/M2BI/KAGGLE/chest_xray/test"
-	path_train = "/Users/rgoulanc/Desktop/Rebecca/FAC/M2BI/KAGGLE/chest_xray/train"
-	path_val = "/Users/rgoulanc/Desktop/Rebecca/FAC/M2BI/KAGGLE/chest_xray/val"
+    #path_test = "/Users/rgoulanc/Desktop/Rebecca/FAC/M2BI/KAGGLE/chest_xray/test"
+    #path_train = "/Users/rgoulanc/Desktop/Rebecca/FAC/M2BI/KAGGLE/chest_xray/train"
+    #path_val = "/Users/rgoulanc/Desktop/Rebecca/FAC/M2BI/KAGGLE/chest_xray/val"
+    path_test = "/home/sdv/m2bi/rgoulancourt/Kaggle/data/archive/chest_xray/test"
+    path_train = "/home/sdv/m2bi/rgoulancourt/Kaggle/data/archive/chest_xray/train"
+    path_val = "/home/sdv/m2bi/rgoulancourt/Kaggle/data/archive/chest_xray/val"
 
-	listing = folder(path_test, path_train, path_val)
-	#listing = list(folder(path_test, path_train, path_val))
-	print(listing)
-
-
-
-
+    test_normal, test_pneu, train_normal, train_pneu, val_normal, val_pneu = folder(path_test, path_train, path_val)
+    df_test, df_train, df_val = as_df(test_normal, test_pneu, train_normal, train_pneu, val_normal, val_pneu)
+    #listing = list(folder(path_test, path_train, path_val))
+    #print(listing)
 
 
 
